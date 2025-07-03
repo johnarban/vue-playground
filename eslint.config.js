dab7fde88ec72a9e0dec17ae9958f4d65a2fba55
John Arban Lewis <johnarban@users.noreply.github.com>
2025-07-03 19:32:14 -0400

    fix linting issues

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,36 @@
-import vuetify from 'eslint-config-vuetify'
+import vuetify from 'eslint-config-vuetify';
+import vue from 'eslint-plugin-vue';
+import pvuetify from 'eslint-plugin-vuetify';
+import { defineConfig } from 'eslint/config';
 
-export default vuetify()
+export default defineConfig(
+  [
+    ...vue.configs['flat/recommended'],
+    ...(await vuetify({ 'perfectionist': false })),
+    {
+
+      plugins: {
+        pvuetify,
+      },
+      rules: {
+        'vue/script-indent': ['error', 2, { baseIndent: 0 }],
+        '@stylistic/no-mixed-operators': 'warn',
+        '@stylistic/quote-props': 'off',
+        '@perfectionist/sort-imports': 'off',
+        '@stylistic/semi': 'off',
+        '@/semi': 'error',
+        '@stylistic/no-trailing-spaces': ['warn', {
+          'skipBlankLines': true,
+          'ignoreComments': true,
+        }],
+        'import/no-duplicates': 'warn',
+      },
+    },
+    
+    {
+      files: ['*.vue'],
+      rules: {
+        indent: 'off',
+      },
+    },
+  ]);
